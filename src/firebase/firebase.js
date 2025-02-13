c493c4d5eea20aa6cdcb85c759158c9eef6053b9
Shoxrux Asadov <129892167+Shoxruxasadov@users.noreply.github.com>
2025-02-13 17:10:58 +0500

    Firebase converted to another project, I needed this firebase project for storage...

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtJdTmtttuHr1SIc_EU1Yi4d_njA2-3WY",
-  authDomain: "regular-school.firebaseapp.com",
-  databaseURL: "https://regular-school-default-rtdb.firebaseio.com",
-  projectId: "regular-school",
-  storageBucket: "regular-school.appspot.com",
-  messagingSenderId: "645303834765",
-  appId: "1:645303834765:web:519e709c0ba4e8492eeac4",
-  measurementId: "G-V4GGJ6NGZC"
+  // apiKey: "AIzaSyCtJdTmtttuHr1SIc_EU1Yi4d_njA2-3WY",
+  // authDomain: "regular-school.firebaseapp.com",
+  // databaseURL: "https://regular-school-default-rtdb.firebaseio.com",
+  // projectId: "regular-school",
+  // storageBucket: "regular-school.appspot.com",
+  // messagingSenderId: "645303834765",
+  // appId: "1:645303834765:web:519e709c0ba4e8492eeac4",
+  // measurementId: "G-V4GGJ6NGZC"
 };
 
 const app = initializeApp(firebaseConfig);
